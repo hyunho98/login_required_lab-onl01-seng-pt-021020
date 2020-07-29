b2910482756f0db6c15b85b42d71ba0e592abb39
Alex Lee <hyunho98@gmail.com>
2020-07-29 07:03:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    session[:name] ||= nil
+    session[:name]
   end
 
   private
